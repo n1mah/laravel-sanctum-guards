00785dbc8c12207f86635850b690525df7c67907
Nima Heidari <nima.heidari.n@gmail.com>
2025-10-29 00:29:45 +0330

    feat: seed initial Admin, Teacher, and Student data in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
+use App\Models\Student;
+use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +23,38 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Admin::create([
+            'name'=> 'Admin',
+            'email'=> 'admin@gmail.com',
+            'password'=>hash('sha256','12345678'),
+            'role'=>1,
+            'mobile'=>'01711111111',
+        ]);
+
+        Teacher::create([
+            'first_name'=>'sara',
+            'last_name'=>'nik',
+            'mobile'=>'0391252313',
+            'gender' => 'female',
+            'email' => 'niksa@yahoo.com',
+            'password'=>hash('sha256','12345678'),
+            'university'=> 'university of jordan',
+            'field_study' => 'computer science',
+            'qualification' => 'master',
+            'experience' => '10 years',
+        ]);
+
+        Student::create([
+            'first_name' => 'ahmed',
+            'last_name' => 'aziik',
+            'mobile' => '0222252313',
+            'gender' => 'male',
+            'field_study' => 'engineering',
+            'email' => 'azik@gmail.com',
+            'password'=>hash('sha256','12345678'),
+            'inter'=>'202403',
         ]);
     }
 }
