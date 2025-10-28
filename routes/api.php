8bd1ab02189e2cfc8677aa5b3b8778ec8b28f416
Nima Heidari <nima.heidari.n@gmail.com>
2025-10-29 00:29:54 +0330

    feat: add routes for Admin, Teacher, and Student token generation and authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Models\Admin;
+use App\Models\Student;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/student/token', function () {
+    return ['token' => Student::findOrFail(1)?Student::findOrFail(1)->createToken('dev')->plainTextToken:null];
+});
+Route::post('/teacher/token', function () {
+    return ['token' => Teacher::findOrFail(1)?Teacher::findOrFail(1)->createToken('dev')->plainTextToken:null];
+});
+Route::post('/admin/token', function () {
+    return ['token' => Admin::findOrFail(1)?Admin::findOrFail(1)->createToken('dev')->plainTextToken:null];
+});
+Route::middleware('auth:students_api')->get('/students/me', fn () => request()->user());
+Route::middleware('auth:teachers_api')->get('/teachers/me', fn () => request()->user());
+Route::middleware('auth:admins_api')->get('/admins/me',   fn () => request()->user());
+
